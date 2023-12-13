5de57526cf23cd6753ca42fc4feee782991aee3f
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-12-13 16:05:13 +0900

    refactor

--- app/[parentId]/page.tsx
+++ app/[parentId]/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
+export default function page({ params }) {
   return (
     <>
-      <Link href="/parent/new">/parent/new</Link>
+      <Link href={`/${params.parentId}/new`}>/{params.parentId}/new</Link>
       <br />
-      <Link href="/parent/1">/parent/1</Link>
+      <Link href={`/${params.parentId}/1`}>/{params.parentId}/1</Link>
     </>
   );
 }
